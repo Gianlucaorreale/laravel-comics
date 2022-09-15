92dee23f48ad916091fda2cf9d953992d7b3d2a9
Gianlucaorreale <gianlucacorreale.1996@gmail.com>
2022-09-15 17:28:06 +0200

    added-includes-folder

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -9,10 +9,11 @@
 <body>
 <header>
        <ul>
+           <li><img src="{{ asset('images/dc-logo.png') }}" alt="dc-logo"></li>
            <li><a href="{{ url('/')}}">CHARACTERS</a></li>
            <li><a href="{{route('comics')}}">COMICS</a></li>
            <li><a href="{{route('movies')}}">MOVIES</a></li>
-           <li><a href="{{route('tv')}}"></a>TV</li>
+           <li><a href="{{route('tv')}}">TV</a></li>
            <li><a href="{{route('games')}}">GAMES</a></li>
            <li><a href="{{route('collectibles')}}">COLLECTIBLES</a></li>
            <li><a href="{{route('videos')}}">VIDEOS</a></li>
--- resources/views/tv.blade.php
+++ resources/views/tv.blade.php
@@ -12,7 +12,7 @@
            <li><a href="{{ url('/')}}">CHARACTERS</a></li>
            <li><a href="{{route('comics')}}">COMICS</a></li>
            <li><a href="{{route('movies')}}">MOVIES</a></li>
-           <li><a href="{{route('tv')}}"></a>TV</li>
+           <li><a href="{{route('tv')}}">TV</a></li>
            <li><a href="{{route('games')}}">GAMES</a></li>
            <li><a href="{{route('collectibles')}}">COLLECTIBLES</a></li>
            <li><a href="{{route('videos')}}">VIDEOS</a></li>
