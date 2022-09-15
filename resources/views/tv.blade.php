22bf0ceff8c21ef658b56789cf75e6959d63eb19
Gianlucaorreale <gianlucacorreale.1996@gmail.com>
2022-09-15 17:52:50 +0200

    added-include

--- resources/views/tv.blade.php
+++ resources/views/tv.blade.php
@@ -7,19 +7,6 @@
     <title>Document</title>
 </head>
 <body>
-<header>
-       <ul>
-           <li><a href="{{ url('/')}}">CHARACTERS</a></li>
-           <li><a href="{{route('comics')}}">COMICS</a></li>
-           <li><a href="{{route('movies')}}">MOVIES</a></li>
-           <li><a href="{{route('tv')}}">TV</a></li>
-           <li><a href="{{route('games')}}">GAMES</a></li>
-           <li><a href="{{route('collectibles')}}">COLLECTIBLES</a></li>
-           <li><a href="{{route('videos')}}">VIDEOS</a></li>
-           <li><a href="{{route('fans')}}">FANS</a></li>
-           <li><a href="{{route('news')}}">NEWS</a></li>
-           <li><a href="{{route('shop')}}">SHOP</a></li>
-       </ul>
-</header>
+    @include('includes.header')
 </body>
 </html>
