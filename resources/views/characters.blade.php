9887d318861d80a2acebd18520de74297176f3e1
Gianlucaorreale <gianlucacorreale.1996@gmail.com>
2022-09-17 12:22:18 +0200

    added-first-header

--- resources/views/characters.blade.php
+++ resources/views/characters.blade.php
@@ -10,19 +10,58 @@
 <body>
     @include('includes.header')
     <footer>
-        <section class="first-sec"></section>
-        <section class="second-sec">
-            <ul>
-                <li><a class="white" href="#"><strong>SIGN-UP NOW</strong></a></li>
-            </ul>
-            <ul>
-                <li><a class="blue" href="#"><strong>FOLLOW US</strong></a></li>
-                <li><a class="blue" href="#"><strong>Facebook</strong></a></li>
-                <li><a class="blue" href="#"><strong>twitter</strong></a></li>
-                <li><a class="blue" href="#"><strong>youtube</strong></a></li>
-                <li><a class="blue" href="#"><strong>pinterest</strong></a></li>
-            </ul>
-        </section>
+        <div class="first-footer">
+            <div class="container">
+                <div class="row">
+                    <div class="first-col">
+                        <div class="first-list-sec">
+                           <ul>
+                              <li><a href="#"><strong>DC COMICS</strong></a></li>
+                              <li><a href="#">Characters</a></li>
+                              <li><a href="#">Comics</a></li>
+                              <li><a href="#">movies</a></li>
+                              <li><a href="#">Tv</a></li>
+                              <li><a href="#">Games</a></li>
+                              <li><a href="#">Videos</a></li>
+                              <li><a href="#">News</a></li>
+                           </ul>
+                           <ul>
+                              <li><a href="#"><strong>SHOP</strong></a></li>
+                              <li><a href="#">Shop DC</a></li>
+                              <li><a href="#">Shop DC Collectibles</a></li>
+                           </ul>
+                        </div>
+                        <div class="second-list-sec">
+                           <ul>
+                              <li><a href="#"><strong>DC</strong></a></li>
+                              <li><a href="#">Terms of use</a></li>
+                              <li><a href="#">Privacy policy(new)</a></li>
+                              <li><a href="#">Ad choices</a></li>
+                              <li><a href="#">Advertising</a></li>
+                              <li><a href="#">jobs</a></li>
+                              <li><a href="#">subscription</a></li>
+                              <li><a href="#">talent Workshop</a></li>
+                              <li><a href="#">CPSC certificates</a></li>
+                              <li><a href="#">Ratings</a></li>
+                              <li><a href="#">shop Help</a></li>
+                              <li><a href="#">Contact Us</a></li>
+                           </ul>
+                           <ul>
+                              <li><a href="#"><strong>SITES</strong></a></li>
+                              <li><a href="#">DC</a></li>
+                              <li><a href="#">MAD magazine</a></li>
+                              <li><a href="#">DC Kids</a></li>
+                              <li><a href="#">DC Universe</a></li>
+                              <li><a href="#">DC Power Visa</a></li>
+                           </ul>
+                        </div>
+                    </div>
+                    <div class="second-col">
+                       <img src="{{ asset('images/dc-logo-bg.png') }}" alt="dc-logo-bg">
+                    </div>
+                </div>
+            </div>
+        </div>
     </footer>
 </body>
 </html>
