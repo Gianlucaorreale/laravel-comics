5ff868ed7eab8fb92dc3baeafbd704d1771b4752
Gianlucaorreale <gianlucacorreale.1996@gmail.com>
2022-09-17 17:39:40 +0200

    added-layouts-folder

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,30 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="{{asset('css/app.css')}}" type ="text/css">
-</head>
-<body>
-<header>
-       <ul>
-           <li><img src="{{ asset('images/dc-logo.png') }}" alt="dc-logo"></li>
-           <li><a href="{{ url('/')}}">CHARACTERS</a></li>
-           <li><a href="{{route('comics')}}">COMICS</a></li>
-           <li><a href="{{route('movies')}}">MOVIES</a></li>
-           <li><a href="{{route('tv')}}">TV</a></li>
-           <li><a href="{{route('games')}}">GAMES</a></li>
-           <li><a href="{{route('collectibles')}}">COLLECTIBLES</a></li>
-           <li><a href="{{route('videos')}}">VIDEOS</a></li>
-           <li><a href="{{route('fans')}}">FANS</a></li>
-           <li><a href="{{route('news')}}">NEWS</a></li>
-           <li><a href="{{route('shop')}}">SHOP</a></li>
-       </ul>
-</header>
-    <div class="jumbotron"></div>
-    <footer>
+<footer>
         <div class="first-footer">
             <div class="container">
                 <div class="row">
@@ -103,5 +77,3 @@
             </div>
         </div>
     </footer>
-</body>
-</html>
