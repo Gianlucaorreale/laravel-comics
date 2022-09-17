26d093357f8f42a8c5ee01452ce37002ac7affb8
Gianlucaorreale <gianlucacorreale.1996@gmail.com>
2022-09-17 19:05:08 +0200

    added-comics-section

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -1,1 +1,17 @@
 @extends('layouts.main')
+
+@section('main-content')
+  <div class="container">
+      <section id="products">
+          <h1>CURRENT SERIES</h1>
+          <div class="row">
+            @foreach($comics as $comics)
+            <div class="card">
+                <img src="{{$comics['thumb']}}" alt="{{$comics['title']}}">
+                <h3>{{$comics['series']}}</h3>
+            </div>
+            @endforeach
+          </div>
+      </section>
+  </div>
+@endsection
